d047c2903735a76b3fe05d4e5ab5b50b338ecda0
Françoise Banier <51740531+fbanier@users.noreply.github.com>
2025-08-22 09:07:44 +0200

    initial commit

---
+++ src/main/java/org/example/rivalry/RivalryApplication.java
@@ -0,0 +1,13 @@
+package org.example.rivalry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivalryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RivalryApplication.class, args);
+    }
+
+}
